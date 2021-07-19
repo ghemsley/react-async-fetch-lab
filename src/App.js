b2e6abcd07f0717430864a3b87fa188a98c21b9d
ghemsley <ghemsley@protonmail.ch>
2021-07-19 05:45:15 -0500

    commit tree 0adc0c6e4ca4fbb567508b83a017f27d2d478b37

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
+import React, {Component} from 'react'
+
 // create your App component here
+class App extends Component {
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+  }
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default App
